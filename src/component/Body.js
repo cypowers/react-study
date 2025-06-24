750ce1911215117339cacec0ebe1aa8e45989deb
cypowers <cypark73@hotmail.com>
2025-06-24 10:21:56 +0900

    여러개의 상태를 한번에

--- src/component/Body.js
+++ src/component/Body.js
@@ -3,46 +3,37 @@ import React, { useState } from 'react';
 
 function Body() {
 
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("");
-    const [option, setOption] = useState("");
-
-    function handleIncClick(e){
-        setCount(count + 1);
-    };
-
-    function handleOnChangeOption(e){
-        setOption(e.target.value);
-        console.log("입력값이 변경되었습니다.", e.target.value);
-        
+    const [state, setState] = useState({
+        name: "",
+        gender: "",
+        birth: "",
+        bio: ""
+    });
+    
+    const handleOnChange = (e) => {
+        console.log("현재 수정 대상: ", e.target.name, "값: ", e.target.value);
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        });
     }
-
-    function handleOnChange(e){
-        setText(e.target.value);
-        console.log("입력값이 변경되었습니다.", e.target.value);
-    }
-
-    function handleClick(e){
-        console.log(e);
-        console.log("버튼이 클릭되었습니다.", e.target.name);
-    };
 
     return (
         <div className="body">
-            <h1>Count is {count}</h1>
-            <p>
-                <input type="date" placeholder="Type here..." onChange={handleOnChange} value={text}/>
-            </p>
-            <p>{text}</p>
-            <button onClick={handleClick} name="A Btn">Click Here11</button>
-            <button onClick={handleClick} name="B Btn">Click Here2</button>
-            <button onClick={handleIncClick} >+</button>
-            <select onChange={handleOnChangeOption} value={option}>
-                <option key="">선택하세요</option>
-                <option key={"apple"}>사과</option>
-                <option key={"banana"}>바나나</option>
-                <option key={"orange"}>오렌지</option>
-            </select>
+            <div><input name="name" value={state.name} onChange={handleOnChange} placeholder="이름"/></div>
+            <div>
+                <select name="gender" value={state.gender} onChange={handleOnChange}>
+                    <option value=""></option>
+                    <option value={"남성"}>남성1</option>
+                    <option value={"여성"}>여성2</option>
+                </select>
+            </div>
+            <div>
+                <input name="birth" type="date" value={state.brirth} onChange={handleOnChange} placeholder="생일"/>
+            </div>
+            <div>
+                <textarea name="bio" value={state.bio} onChange={handleOnChange}></textarea>
+            </div>
         </div>
             
     );
